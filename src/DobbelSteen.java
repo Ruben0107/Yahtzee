28ead346df66264c91ce2c05b283b5ec3f01826c
Ruben0107 <rubenvanrij@gmail.com>
2020-02-26 15:54:51 +0100

    go4

--- src/DobbelSteen.java
+++ src/DobbelSteen.java
@@ -1,17 +1,13 @@
 import java.util.Random;
 
 public class DobbelSteen extends YahtzeeSpel {
-	
-static int werpen() {
-		
-		
+
+	static int werpen() {
+
 		Random r = new Random();
-		
-		int r1 = (r.nextInt(6))+1;
-		
-		return r1; 
-		
+		int r1 = (r.nextInt(6)) + 1;
+		return r1;
+
 	}
 
 }
-
